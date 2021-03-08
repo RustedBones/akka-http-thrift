194e6215583e90459861e7e8534cd861046a56a0
Scala Steward <me@scala-steward.org>
2021-03-08 08:29:24 +0100

    Update scalatest to 3.2.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val akka      = "2.6.13"
     val akkaHttp  = "10.2.4"
     val logback   = "1.2.3"
-    val scalaTest = "3.2.5"
+    val scalaTest = "3.2.6"
     val thrift    = "0.14.0"
     val scrooge   = "19.11.0"
   }
