bcaa0c6bcd2c6451a2533416c6dce734458002a8
Scala Steward <me@scala-steward.org>
2021-08-19 14:13:21 +0200

    Update akka-stream, akka-testkit to 2.6.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val akka      = "2.6.15"
+    val akka      = "2.6.16"
     val akkaHttp  = "10.2.6"
     val logback   = "1.2.5"
     val scalaTest = "3.2.8"
