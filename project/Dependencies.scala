14296f39d6113d558b8bc1258a566ea1508d8d4d
Scala Steward <me@scala-steward.org>
2021-09-12 06:50:59 +0200

    Update libthrift to 0.15.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val akkaHttp  = "10.2.6"
     val logback   = "1.2.5"
     val scalaTest = "3.2.8"
-    val thrift    = "0.14.2"
+    val thrift    = "0.15.0"
     val scrooge   = "21.6.0"
   }
 
